83666b47296b5b8a9bd53b21ed0e5adf5e67e230
Aditya Sawant <129196467+Catalyst3@users.noreply.github.com>
2023-06-08 17:43:46 +0530

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,32 +1,37 @@
-
+package ByUser;
+import java.util.Scanner;
 public class Bank 
 {
-	void withdrawal()
-	{
-		System.out.println("Enter your pin");
-		System.out.println("Enter ammount");
-	}
-	void deposit()
-	{
-		System.out.println("Enter your pin");
-		System.out.println("Enter account number");
-		System.out.println("Enter ammount");
-	}
-	void balanceInquiry()
-	{
-		System.out.println("Enter your pin");
-		System.out.println("Emter account number");
-	}
-	void openBankAccount()
-	{
-		System.out.println("Enter your name");
-		System.out.println("Enter your address");
-		System.out.println("Enter your Phone number");
-	}
-	public static void main(String args[])
-	{
-		Bank a2 = new Bank();
-		a2.openBankAccount();
-	}
-
+	int balance=100000;
+   void checkBalance()
+   {
+	   System.out.println("Your balance is "+balance);
+	   System.out.println("---------------------------------");  
+   }
+   void withdraw(int amount)
+   { 
+	   balance = balance - amount;
+	   System.out.println("---------------------------------");
+	   System.out.println("Your balance after withdraw is :"+balance);
+	   System.out.println("---------------------------------");
+   }
+   void deposit(int amount)
+   {
+	   balance = balance + amount;
+	   System.out.println("---------------------------------");
+	   System.out.println("Your balance after deposit is :"+balance);  
+	   System.out.println("---------------------------------");
+   }
+   public static void main(String args[])
+   {
+	   Bank a = new Bank();
+	   a.checkBalance();
+	   System.out.println("Enter ammount you want to withdraw"); 
+	   Scanner sc = new Scanner(System.in);
+	   int amount = sc.nextInt();
+	   a.withdraw(amount);
+	   System.out.println("Enter ammount you want to deposit");
+	   amount = sc.nextInt();
+	   a.deposit(amount);
+   }
 }
